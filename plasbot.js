80d3fb3d456f1f4c8659d4a91ac33e4ea44a1d71
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-13 08:55:07 +0100

    Update plasbot.js

--- plasbot.js
+++ plasbot.js
@@ -54,6 +54,24 @@ bot.on("message", async message => {
 
     message.channel.send(embed)
   }
+  if (message.content === "p!userinfo"){
+    let member = message.mentions.users.first() || message.author;
+    var embed = new Discord.RichEmbed()
+    
+    .setColor(message.guild.member(member).highestRole.color)
+    .setThumbnail(member.displayAvatarURL)
+        
+    .setTitle(`Here is ${member.username}'s info.`)
+    .addField(`Name:`, member.username, true)
+    .addField(`Id:`, member.id, true)
+    .addField(`Bot:`, member.bot ? "Yes" : "No", true)
+    .addField("Game:", message.guild.member(member).presence.game ? message.guild.member(member).presence.game.name : "Not Playing", true) // the ? and : are like an if statement if (msg.guild.member(member).presence.game ) { msg.guild.member(member).presence.game.name } else "Not Playing"
+    .addField("Nickname:", message.guild.member(member).nickname ? message.guild.member(member).nickname : "None", true )
+    .addField("Last Messsage:", member.lastMessage, true)
+    .addField(`Roles:`, message.guild.member(member).roles.map(s => s).join(" | "), true);
+    message.channel.send(embed)
+  }
+  
 });
 
 bot.login(process.env.token);
